85993a4695ca7877b16b80dd6efee9ba0031cf22
“gursham” <Gursham2001@hotmail.com>
2022-05-09 16:56:38 +0100

    1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,181 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import NumberFormat from "react-number-format";
 
 function App() {
+
+  // we have to set us useState to set the state at the start before anything occurs
+  const [preState, setPreState] = useState("");
+  const [curState, setCurState] = useState("");
+  const [input, setInput] = useState("0");
+  const [operator, setOperator] = useState(null);
+  const [total, setTotal] = useState(false);
+
+  const inputNum = (e) => {
+    // will not allow us to type two dots .
+    if (curState.includes(".") && e.target.innerText === ".") return;
+    // we set previous input to nothing because when the equals button is pressed we dont want the calculations to keep running and giving different answers.
+    if (total) {
+      setPreState("");
+    }
+    // this means we can press the same button multiple times
+    curState
+      ? setCurState((pre) => pre + e.target.innerText)
+      : setCurState(e.target.innerText);
+    setTotal(false);
+  };
+  // every time current state changes we set the current state to the current state
+  useEffect(() => {
+    setInput(curState);
+  }, [curState]);
+
+  useEffect(() => {
+    setInput("0");
+  }, []);
+  const operatorType = (e) => {
+    // takes in and register the event
+    // setting total to false lets the operator know we want to continue the calculations
+    setTotal(false);
+    setOperator(e.target.innerText);
+    // we do this to check if there is any input so fa
+    if (curState === "") return;
+    // we check if we have a previous input if so we want to preform calculation hence equals()
+    if (preState !== "") {
+      equals();
+    } else {
+      setPreState(curState);
+    // setting previous input to current input so we can continue calcs
+    // making setCurrentInput a black string means that will be a blank input area for more calcs
+      setCurState("");
+    }
+  };
+
+  const equals = (e) => {
+    if (e?.target.innerText === "=") {
+      setTotal(true);
+    }
+    let cal;
+    switch (operator) {
+      case "/":
+        cal = String(parseFloat(preState) / parseFloat(curState));
+        break;
+
+      case "+":
+        cal = String(parseFloat(preState) + parseFloat(curState));
+        break;
+      case "X":
+        cal = String(parseFloat(preState) * parseFloat(curState));
+        break;
+      case "-":
+        cal = String(parseFloat(preState) - parseFloat(curState));
+        break;
+      default:
+        return;
+    }
+    setInput("");
+    setPreState(cal);
+    setCurState("");
+  };
+
+  // checks if the plus and minus button then displays if so
+  const minusPlus = () => {
+    if (curState.charAt(0) === "-") {
+      setCurState(curState.substring(1));
+    } else {
+      setCurState("-" + curState);
+    }
+  };
+  // this is the calculation to get the percent
+  const percent = () => {
+    preState
+      ? setCurState(String((parseFloat(curState) / 100) * preState))
+      : setCurState(String(parseFloat(curState) / 100));
+  };
+  // we set everything back to the original use state to reset
+  const reset = () => {
+    setPreState("");
+    setCurState("");
+    setInput("0");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <h1>Clock</h1>
+    <div className='container'>
+      <div className='wrapper'>
+        <div className='screen'>
+          {input !== "" || input === "0" ? (
+            <NumberFormat
+              value={input}
+              displayType={"text"}
+              thousandSeparator={true}
+            />
+          ) : (
+            <NumberFormat
+              value={preState}
+              displayType={"text"}
+              thousandSeparator={true}
+            />
+          )}
+        </div>
+        <div className='btn light-gray' onClick={reset}>
+          AC
+        </div>
+        <div className='btn light-gray' onClick={percent}>
+          %
+        </div>
+        <div className='btn light-gray' onClick={minusPlus}>
+          +/-
+        </div>
+        <div className='btn orange' onClick={operatorType}>
+          /
+        </div>
+        <div className='btn' onClick={inputNum}>
+          7
+        </div>
+        <div className='btn' onClick={inputNum}>
+          8
+        </div>
+        <div className='btn' onClick={inputNum}>
+          9
+        </div>
+        <div className='btn orange' onClick={operatorType}>
+          X
+        </div>
+        <div className='btn' onClick={inputNum}>
+          4
+        </div>
+        <div className='btn' onClick={inputNum}>
+          5
+        </div>
+        <div className='btn' onClick={inputNum}>
+          6
+        </div>
+        <div className='btn orange' onClick={operatorType}>
+          +
+        </div>
+        <div className='btn' onClick={inputNum}>
+          1
+        </div>
+        <div className='btn' onClick={inputNum}>
+          2
+        </div>
+        <div className='btn' onClick={inputNum}>
+          3
+        </div>
+        <div className='btn orange' onClick={operatorType}>
+          -
+        </div>
+        <div className='btn zero' onClick={inputNum}>
+          0
+        </div>
+        <div className='btn' onClick={inputNum}>
+          .
+        </div>
+        <div className='btn' onClick={equals}>
+          =
+        </div>
+      </div>
     </div>
+    </>
   );
 }
 
